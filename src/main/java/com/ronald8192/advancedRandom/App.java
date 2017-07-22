645bb501b3e45af06a3dee2b9a5bd19cb35f03ee
ronald8192 <u23805@gmail.com>
2017-07-22 17:51:50 +0800

    init

---
+++ src/main/java/com/ronald8192/advancedRandom/App.java
@@ -0,0 +1,17 @@
+package com.ronald8192.advancedRandom;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+//		System.out.println("Hello World!");
+		Logger log = LoggerFactory.getLogger(App.class);
+		log.info("HelloWorld");
+		log.debug("HelloWorld!");
+	}
+}
